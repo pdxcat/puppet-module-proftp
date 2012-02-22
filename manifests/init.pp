5d439bd042a1d3772dfd3db6b13704db6a2a2ca4
William Van Hevelingen <blkperl@cat.pdx.edu>
2012-02-22 10:44:53 -0800

    Adding files, package, service resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,43 @@
 class proftpd {
 
+  user { 'ftp':
+    ensure   => 'present',
+    comment  => 'Anonymous Ftp',
+    gid      => '1',
+    home     => '/u/ftp',
+    password => '*',
+    shell    => '/bin/sh',
+    uid      => '8',
+  }
+
+  File {
+    owner     => root,
+    group     => root,
+    mode      => 644,
+  }
+
+  file {
+    "proftpd-preseed":
+      path    => "/var/cache/debconf/proftpd",
+      source  => "puppet://$server/modules/proftpd/proftpd-preseed";
+
+    "/etc/proftpd/proftpd.conf":
+      source  => "puppet://$server/modules/proftpd/etc/proftpd/proftpd.conf";
+
+    "/etc/pam.d/proftpd":
+      source  => "puppet://$server/modules/proftpd/etc/pam.d/proftpd";
+   }
+
+  package { "proftpd":
+    ensure    => present,
+    require   => File["proftpd-preseed"]
+  }
+
+  service { "proftpd":
+    ensure    => running,
+    enable    => true,
+    subscribe => File["/etc/proftpd/proftpd.conf"],
+    require   => File["/etc/pam.d/proftpd"]
+  }
 
 }
